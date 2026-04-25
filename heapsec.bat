5df83f6582c14a52fc1e394cb4550cfb061bc7d0
FelipeMenezes937 <felipemenezes937@gmail.com>
2026-04-25 18:02:14 -0300

    fix: windows compatibility 2

--- heapsec.bat
+++ heapsec.bat
@@ -5,45 +5,23 @@ REM Compatible with Java 11+
 set SCRIPT_DIR=%~dp0
 set CLASSES_DIR=%SCRIPT_DIR%out\classes
 
-set JAVA_HOME_DETECTED=
 set JAVAC_PATH=
 
-if defined JAVA_HOME (
-    if exist "%JAVA_HOME%\bin\javac.exe" (
-        set JAVA_HOME_DETECTED=%JAVA_HOME%
+where java >nul 2>nul
+if %errorlevel% equ 0 (
+    for /f "delims=" %%i in ('where java') do (
+        set JAVA_BIN=%%i
+        goto :found_java
+    )
+    :found_java
+    if not "%JAVA_BIN%"=="" (
+        set JAVAC_PATH=%JAVA_BIN:~0,-8%
+    )
+)
+
+if not defined JAVAC_PATH (
+    if defined JAVA_HOME (
         set JAVAC_PATH=%JAVA_HOME%\bin
-    )
-)
-
-if not defined JAVAC_PATH (
-    for /d %%d in ("%ProgramFiles%\Java\*") do (
-        if exist "%%d\bin\javac.exe" (
-            set JAVA_HOME_DETECTED=%%d
-            set JAVAC_PATH=%%d\bin
-        )
-    )
-)
-
-if not defined JAVAC_PATH (
-    for /d %%d in ("%ProgramFiles(x86)%\Java\*") do (
-        if exist "%%d\bin\javac.exe" (
-            set JAVA_HOME_DETECTED=%%d
-            set JAVAC_PATH=%%d\bin
-        )
-    )
-)
-
-if not defined JAVAC_PATH (
-    where java >nul 2>nul
-    if not errorlevel 1 (
-        for /f "delims=" %%i in ('where java') do (
-            set JAVA_BIN=%%i
-            goto :found_java
-        )
-        :found_java
-        if not "%JAVA_BIN%"=="" (
-            set JAVAC_PATH=%JAVA_BIN:~0,-8%
-        )
     )
 )
 
@@ -55,11 +33,11 @@ if not defined JAVAC_PATH (
 "%JAVAC_PATH%\java.exe" -version 2>&1 | findstr /C:"version" >nul
 for /f "delims=" %%v in ('"%JAVAC_PATH%\java.exe" -version 2^>^&1') do set JAVA_VERSION_LINE=%%v
 echo %JAVA_VERSION_LINE% | findstr /C:"11" >nul
-if errorlevel 1 (
+if %errorlevel% neq 0 (
     echo %JAVA_VERSION_LINE% | findstr /C:"17" >nul
-    if errorlevel 1 (
+    if %errorlevel% neq 0 (
         echo %JAVA_VERSION_LINE% | findstr /C:"21" >nul
-        if errorlevel 1 (
+        if %errorlevel% neq 0 (
             echo ERROR: Java 11+ required. Current version:
             echo %JAVA_VERSION_LINE%
             exit /b 1
@@ -73,7 +51,7 @@ echo Compiling HeapSec...
 if not exist "%CLASSES_DIR%" mkdir "%CLASSES_DIR%"
 
 "%JAVAC_PATH%\javac.exe" -d "%CLASSES_DIR%" -sourcepath src\main\java src\main\java\antivirus\*.java src\main\java\antivirus\*\*.java 2>NUL
-if errorlevel 1 (
+if %errorlevel% neq 0 (
     "%JAVAC_PATH%\javac.exe" -d "%CLASSES_DIR%" -sourcepath src\main\java src\main\java\antivirus\*.java src\main\java\antivirus\*\*.java
 )
 
